ba44540c29a36c0b8ea175b00c03dc77cec34575
Rob Struthers <rob.struthers@hydrix.com>
2015-08-06 18:43:35 +1000

    Add info to the build script

--- do_build.sh
+++ do_build.sh
@@ -10,7 +10,27 @@ cd $base
 
 # Configure, build and install
 cd PROJECTS
+
+echo "***"
+echo "***"
+echo "*** Running 'configure'..."
+echo "***"
+echo "***"
 ./configure
+
+echo "***"
+echo "***"
+echo "*** Running 'make'..."
+echo "***"
+echo "***"
 make
+
+echo "***"
+echo "***"
+echo "*** Running 'make install'..."
+echo "***"
+echo "***"
+echo "*** If this fails, add `whoami` to /etc/sudoers; (I am `who am i`)"
+echo "***          `whoami`   ALL = NOPASSWD: ALL"
 sudo make install
 
