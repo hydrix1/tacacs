e824f42a9cabb05aa2eb3a43160ca6ed96677639
Rob Struthers <rob.struthers@hydrix.com>
2015-08-07 10:04:49 +1000

    Add more info to the build script

--- do_build.sh
+++ do_build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/bash
 
 # Find out where we are
-iam=$0
-offset=`dirname $iam`
+script_name=$0
+username=`whoami
+offset=`dirname $script_name`
 called_from=`pwd`
 base=$called_from/$offset
-echo "I am [$iam]; here is [$offset]; wd is [$called_from]; base is [$base]"
 cd $base
+
+echo "***"
+echo "***"
+echo "*** This is $username running $script_name"
+echo "***   -- called from $called_from"
+echo "***   -- working in  $base"
+echo "***"
+echo "***"
 
 # Configure, build and install
 cd PROJECTS
@@ -30,7 +38,15 @@ echo "***"
 echo "*** Running 'make install'..."
 echo "***"
 echo "***"
-echo "*** If this fails, add `whoami` to /etc/sudoers; (I am `who am i`)"
-echo "***          `whoami`   ALL = NOPASSWD: ALL"
+echo "*** If this fails, add $username to /etc/sudoers:"
+echo "***          Defaults:$username   !requiretty"
+echo "***          $username            ALL=(ALL) NOPASSWD:ALL"
+echo "***"
 sudo make install
+
+echo "***"
+echo "***"
+echo "*** $username finished running $script_name"
+echo "***"
+echo "***"
 
