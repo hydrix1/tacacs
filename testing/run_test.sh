e8f5a87d7579a2d2af417e93a6d0a9b74dc36536
Rob Struthers <rob.struthers@hydrix.com>
2015-08-28 09:37:19 +1000

    Improvements to the remote script, including JUnit-friendly output

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -9,13 +9,20 @@ test_log=test.$$.out
 
 
 
+mark_time()
+{
+    time_now=`date +%s.%N`
+    echo "@@@ $time_now"
+}
+
+
 # Define Unity style helper functions
 unity_start()
 {
     declare -a test_groups
     test_depth=0
     test_context=""
-    echo "### UNITY STARTING ###"
+    echo "### TACACS+ TEST SCRIPT STARTING ###"
     test_total=0
     test_fails=0
     test_skips=0
@@ -32,7 +39,7 @@ unity_end()
     while [ ${#padded} -lt $test_max_length ]; do
         padded="$padded "
     done
-    echo "### UNITY FINISHED ###"
+    echo "### TEST SCRIPT FINISHED ###"
     echo "$test_total Tests $test_fails Failures $test_skips Ignored"
     echo "OK"
     echo ""
@@ -84,6 +91,8 @@ unity_start_test()
     skip_test=0
     fail_test=0
     test_name=$1
+    mark_time
+    echo "%%% $test_name"
 }
 
 unity_end_test()
@@ -108,6 +117,7 @@ unity_end_test()
     test_names[$test_total]="$test_title"
     test_results[$test_total]="$test_result"
     test_total=$(($test_total + 1))
+    mark_time
     echo ":::$test_title::: $test_result"
 }
 
@@ -123,6 +133,9 @@ unity_assert_equal()
     fi
 }
 
+
+
+##################################################################################################
 
 
 # Local, tacacs test specific, unity extensions
@@ -150,7 +163,17 @@ tacats_expect_false()
 
 
 
-tacacs_test_auth_good()
+tacats_expect_nothing()
+{
+    tacacs_expect_result ""
+}
+
+
+
+##################################################################################################
+
+
+tacacs_test_authenicate_good()
 {
     unity_start_test "good"
         "$tactest" -s $machine -port $tacacs_port -u readonly -p readonly -k cisco -authen 2>&1 | tee $test_log
@@ -160,43 +183,172 @@ tacacs_test_auth_good()
 
 
 
-tacacs_test_auth_password()
+tacacs_test_authenicate_username()
 {
-    unity_start_test "bad_password"
-        "$tactest" -s $machine -port $tacacs_port -u readonly -p readwrite -k cisco -authen 2>&1 | tee $test_log
+    unity_start_test "bad_username"
+        "$tactest" -s $machine -port $tacacs_port -u rubbish -p readonly -k cisco -authen 2>&1 | tee $test_log
         tacats_expect_false
     unity_end_test
 }
 
 
 
+tacacs_test_authenicate_password()
+{
+    unity_start_test "bad_password"
+        "$tactest" -s $machine -port $tacacs_port -u readonly -p rubbish -k cisco -authen 2>&1 | tee $test_log
+        tacats_expect_false
+    unity_end_test
+}
+
+
+
+tacacs_test_authenicate_key()
+{
+    unity_start_test "bad_key"
+        "$tactest" -s $machine -port $tacacs_port -u readonly -p readonly -k rubbish -authen 2>&1 | tee $test_log
+        tacats_expect_nothing
+    unity_end_test
+}
+
+
+
+##################################################################################################
+
+
+tacacs_test_acct_good()
+{
+    unity_start_test "good"
+        "$tactest" -s $machine -port $tacacs_port -u readonly -k cisco -acct start one=1 2>&1 | tee $test_log
+        tacats_expect_true
+    unity_end_test
+}
+
+
+
+##################################################################################################
+
+
+tacacs_test_authorise_good()
+{
+    unity_start_test "good"
+        "$tactest" -s $machine -port $tacacs_port -u readonly -k cisco -author 2>&1 | tee $test_log
+        tacats_expect_true
+    unity_end_test
+}
+
+
+
+##################################################################################################
+
+
 tacacs_test_authentication()
 {
     unity_start_group "authentication"
-        tacacs_test_auth_good
-        tacacs_test_auth_password
+        tacacs_test_authenicate_good
+        tacacs_test_authenicate_username
+        tacacs_test_authenicate_password
+        tacacs_test_authenicate_key
     unity_end_group
 }
 
 
 
-tacacs_test_service()
+tacacs_test_accounting()
 {
+    unity_start_group "accounting"
+        tacacs_test_acct_good
+    unity_end_group
+}
+
+
+
+tacacs_test_authorisation()
+{
+    unity_start_group "authorisation"
+        tacacs_test_authorise_good
+    unity_end_group
+}
+
+
+
+##################################################################################################
+
+tacacs_test_simple_authenicate_good()
+{
+    unity_start_test "good"
+        "$tactest" -s $machine -port $tacacs_port -u test_4900_a -p A4900 -k cisco -authen 2>&1 | tee $test_log
+        tacats_expect_true
+    unity_end_test
+}
+
+
+
+tacacs_test_simple_authenicate_permit()
+{
+    unity_start_test "permit"
+        "$tactest" -s $machine -port $tacacs_port -u test_4900_b -p any_old_rubbish -k cisco -authen 2>&1 | tee $test_log
+        tacats_expect_true
+    unity_end_test
+}
+
+
+
+tacacs_test_simple_authenicate_deny()
+{
+    unity_start_test "deny"
+        "$tactest" -s $machine -port $tacacs_port -u test_4900_c -p A4900 -k cisco -authen 2>&1 | tee $test_log
+        tacats_expect_false
+    unity_end_test
+}
+
+
+
+tacacs_test_simple_authentication()
+{
+    unity_start_group "authentication"
+        tacacs_test_simple_authenicate_good
+        tacacs_test_simple_authenicate_permit
+        tacacs_test_simple_authenicate_deny
+    unity_end_group
+}
+
+
+##################################################################################################
+
+
+tacacs_test_simple()
+{
+    tacacs_port=$1
+    service=$2
     echo " -- Testing $service (port $tacacs_port)"
     unity_start_group "$service"
-        tacacs_test_authentication
+        tacacs_test_simple_authentication
+        #tacacs_test_accounting
+        #tacacs_test_authorisation
     unity_end_group
 }
 
+
+
+##################################################################################################
 
 
 tacacs_test_server()
 {
     tacacs_port=$1
     service=$2
-    tacacs_test_service
+    echo " -- Testing $service (port $tacacs_port)"
+    unity_start_group "$service"
+        tacacs_test_authentication
+        tacacs_test_accounting
+        tacacs_test_authorisation
+    unity_end_group
 }
 
+
+
+##################################################################################################
 
 
 # Find out provenance and other details
@@ -215,6 +367,7 @@ unity_start
 
 
 # Test 1: the reference server
+tacacs_test_simple 4900 "Simple"
 tacacs_test_server 4949 "Reference"
 tacacs_test_server 4950 "CLargs"
 
