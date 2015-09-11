d78c32df593aa6599d83c569b2222aad79619f04
Rob Struthers <rob.struthers@hydrix.com>
2015-09-11 18:47:48 +1000

    Starting to write first wrapped test

---
+++ testing/unit/string_append.c
@@ -0,0 +1,13 @@
+/*
+ * Unit test program for string_append()
+ */
+
+#include <stdlib.h>
+#include "unity.h"
+#include "wrap_spawnd_main.c"
+
+
+test_entry()
+{
+}
+
---
+++ testing/unit/wrap_fixup.c
@@ -0,0 +1,52 @@
+/*
+ * Fix-up missing items to allow wrapped test code to link.
+ */
+
+int XXstrdup;
+int io_sched_renew_proc;
+int common_data;
+int logmsg;
+int common_data;
+int spawnd_cleanup_internal;
+int spawnd_cleanup_tracking;
+int spawnd_process_signals;
+int XXcalloc;
+int XXrealloc;
+int io_sched_del;
+int su_socket;
+int logerr;
+int io_sched_add;
+int su_bind;
+int su_get_port;
+int su_ntop;
+int logmsg;
+int su_get_port;
+int su_ntop;
+
+int io_register;
+int spawnd_accepted;
+int io_set_cb_i;
+int io_clr_cb_o;
+int io_set_cb_e;
+int io_set_cb_h;
+int io_set_i;
+int init_common_data;
+int logopen;
+int scm_fatal;
+int common_usage;
+int spawnd_spawn_child;
+int ipc_create;
+int spawnd_parse_decls;
+int cfg_read_config;
+int setproctitle_init;
+int pid_write;
+int io_init;
+int fakescm_send_msg;
+int fakescm_recv_msg;
+int spawnd_setup_signals;
+int setup_sig_segv;
+int spawnd_add_child;
+int set_proctitle;
+int io_main;
+int init_common_data;
+int logopen;
