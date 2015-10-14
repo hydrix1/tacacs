5e52532ad3f6af1fe2fc6d3008d61591c7349ce4
Rob Struthers <rob.struthers@hydrix.com>
2015-10-13 23:02:55 -0700

    Add get_optional_argument unit test

--- testing/do_unit_test.sh
+++ testing/do_unit_test.sh
@@ -11,9 +11,42 @@ echo "***"
 
 ###########################################################################
 
-#unity_start_group "Unit"
+# List the unit tests
+unit_tests="string_append
+            get_missing_argument
+            get_omitted_argument
+            get_required_argument
+            get_optional_argument
+            get_next_option
+            parse_listen_subopts
+            patse_cli_listen
+            set_password
+            parse_level_subopts
+            parse_host_subopts
+            parse_cli_debug
+            parse_cli_host
+            parse_cmd_subopts
+            parse_group_subopts
+            parse_cli_group
+            get_user_shell
+            parse_user_subopts
+            parse_cli_user
+            generate_more
+            generate_listen_request
+            generate_spawnd_config
+            generate_level_config
+            generate_level_set_config
+            generate_host_config
+            generate_access_config
+            generate_cmd_config
+            generate_service_config
+            generate_group_config
+            generate_user_config
+            generate_tacplus_config
+            generate_cli_config
+           "
 
-# Run the unit tests themselves
+# Sanity check
 unity_start_test "dummy"
     if [ -x unit/dummy ]; then
 	unit/dummy
@@ -29,7 +62,6 @@ unity_start_test "dummy"
 unity_end_test
 
 # Run the unit tests themselves
-unit_tests="string_append"
 for unit_test in $unit_tests
 do
     unity_start_test "$unit_test"
@@ -46,6 +78,4 @@ do
 	fi
     unity_end_test
 done
-
-#unity_end_group
 
