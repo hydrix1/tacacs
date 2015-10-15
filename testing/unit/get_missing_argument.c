f0bb9ff224d54ed1db1ff36e7282448307a96099
Rob Struthers <rob.struthers@hydrix.com>
2015-10-14 20:35:48 -0700

    Finish get_missing_argument.c

--- testing/unit/get_missing_argument.c
+++ testing/unit/get_missing_argument.c
@@ -32,29 +32,37 @@ static int trace_system(const char* cmd)
 
 
 
-static int catch_test_get_missing_argument(const char* file_base,
-                                           const char* prompt, ...)
+static void catch_test_get_missing_argument(const char* test_name,
+                                            const char* prompt, ...)
 {
     pid_t parent;
     pid_t child;
-    int   result = 0;
+    char* file_base = 0;
     char* in_file = 0;
+    char* expected_result = 0;
     char* expected_output = 0;
     char* expected_error = 0;
+    char* actual_result = 0;
     char* actual_output = 0;
     char* actual_error = 0;
     char* make_actual_dir = 0;
+    char* cmp_results = 0;
     char* cmp_outputs = 0;
     char* cmp_errors = 0;
 
+    unity_start_test(test_name);
     parent = getpid();
 
+    asprintf(&file_base, "get_missing_argument_%s", test_name);
     asprintf(&in_file, "unit/inputs/%s.input", file_base);
+    asprintf(&expected_result, "unit/expect/%s.result", file_base);
     asprintf(&expected_output, "unit/expect/%s.output", file_base);
     asprintf(&expected_error, "unit/expect/%s.error", file_base);
+    asprintf(&actual_result, "unit/got_%d/%s.result", parent, file_base);
     asprintf(&actual_output, "unit/got_%d/%s.output", parent, file_base);
     asprintf(&actual_error, "unit/got_%d/%s.error", parent, file_base);
     asprintf(&make_actual_dir, "mkdir -p unit/got_%d", parent);
+    asprintf(&cmp_results, "diff -bw %s %s", expected_result, actual_result);
     asprintf(&cmp_outputs, "diff -bw %s %s", expected_output, actual_output);
     asprintf(&cmp_errors, "diff -bw %s %s", expected_error, actual_error);
 
@@ -66,7 +74,6 @@ static int catch_test_get_missing_argument(const char* file_base,
         /* fork() failed! */
         fprintf(stderr, "fork() failed!\n");
         unity_fail();
-        result = -1;
     }
     else if (child == 0)
     {
@@ -74,6 +81,7 @@ static int catch_test_get_missing_argument(const char* file_base,
         char* result;
         va_list args;
 
+        unlink (actual_result);
         freopen (in_file, "r", stdin);
         freopen (actual_output, "w", stdout);
         freopen (actual_error, "w", stderr);
@@ -82,7 +90,9 @@ static int catch_test_get_missing_argument(const char* file_base,
         result = get_missing_argument(prompt, args);
         va_end (args);
 
-        // We were not suppoed to reach here!
+        /* We were not suppoed to reach here! */
+        freopen (actual_result, "w", stdout);
+        printf ("%s\n", result);
         exit(0);
     }
     else
@@ -90,14 +100,42 @@ static int catch_test_get_missing_argument(const char* file_base,
         /* we are the parent */
         int status = 0;
         int delta;
+        int expect_to_work = 0;
+        struct stat file_stat;
+
+        /* Do we expect this oprtation to work? */
+        delta = stat(expected_result, &file_stat);
+        if (delta < 0)
+        {
+            /* No expected result file ==> expect failure */
+             expect_to_work = 0;
+        }
+        else
+        {
+            /* There ia an expected result file ==> expect success */
+             expect_to_work = 1;
+        }
+
+        /* wait for the child to exit */
         pid_t from = waitpid (child, &status, 0);
-        fprintf(stderr, "waitpid(%d) response from %d\n", child, from);
+        /* confirm status is from our child */
+        //fprintf(stderr, "waitpid(%d) response from %d\n", child, from);
         unity_assert_int_equal(child, from);
-        fprintf(stderr, "wait() exited with code %d\n", status);
+        //fprintf(stderr, "wait() exited with code %d\n", status);
         /* Check exit status is "exited" ...*/
         unity_assert_int_equal(1, WIFEXITED(status));
-        /* ... and that the return value is non-zero */
-        unity_assert_int_not_equal(0, WEXITSTATUS(status));
+        /* ... and that the return value is as expected */
+        if (expect_to_work)
+        {
+            unity_assert_int_equal(0, WEXITSTATUS(status));
+            delta = trace_system(cmp_results);
+            unity_assert_int_equal(0, delta);
+        }
+        else
+        {
+            unity_assert_int_not_equal(0, WEXITSTATUS(status));
+        }
+
         /* Confirm the output and error files are as expected */
         delta = trace_system(cmp_outputs);
         unity_assert_int_equal(0, delta);
@@ -105,42 +143,35 @@ static int catch_test_get_missing_argument(const char* file_base,
         unity_assert_int_equal(0, delta);
     }
 
-    return result;
-}
+    free(file_base);
+    free(in_file);
+    free(expected_result);
+    free(expected_output);
+    free(expected_error);
+    free(actual_result);
+    free(actual_output);
+    free(actual_error);
+    free(make_actual_dir);
+    free(cmp_results);
+    free(cmp_outputs);
+    free(cmp_errors);
 
-
-
-void test_get_missing_argument_eof()
-{
-    static const char* prompt1 = "First prompt";
-    static const char* file_1 =  "get_missing_argument_eof_nothing";
-    int result;
-
-    unity_start_group("EOF");
-
-    unity_start_test("nothing");
-    result = catch_test_get_missing_argument(file_1, prompt1);
-    //unity_assert_ptr_not_equal (base_1, 0);
-   //unity_assert_ptr_not_equal (base_1, easy);
-    //unity_assert_str_equal (base_1, easy);
     unity_end_test();
-
-    unity_end_group();
-}
-
-
-
-void test_get_missing_argument()
-{
-    unity_start_group("get_missing_argument");
-    test_get_missing_argument_eof();
-    unity_end_group();
 }
 
 
 
 void test_entry()
 {
-    test_get_missing_argument();
+    /* Test get_missing_argument() */
+    unity_start_group("get_missing_argument");
+    catch_test_get_missing_argument("one_word", "one word prompt");
+    catch_test_get_missing_argument("four_words", "four words prompt");
+    catch_test_get_missing_argument("padded", "padded prompt");
+    catch_test_get_missing_argument("spaced", "spaced prompt");
+    catch_test_get_missing_argument("eventually", "eventually prompt");
+    catch_test_get_missing_argument("eof_nothing", "First prompt");
+    catch_test_get_missing_argument("eof_spaces", "second prompt");
+    unity_end_group();
 }
 
